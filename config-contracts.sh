b4262b62f5cc2dab4ffb32144a101f3e64a9f7fd
vrde <agranzot@mailbox.org>
2022-02-15 12:39:02 +0100

    Add submodule

--- config-contracts.sh
+++ config-contracts.sh
@@ -1,30 +1,20 @@
 #!/usr/bin/env bash
 
-CONTRACTS_DIR="../contracts/";
+CONTRACTS_DIR="contracts";
 
-# Check if contracts dir exists
-
-if [[ ! -d ${CONTRACTS_DIR} ]]
-then
-    echo "Cannot find contracts in ${CONTRACTS_DIR}."
-    echo "Checkout the git repo: https://github.com/TelediskoDAO/contracts"
-    echo "in ${CONTRACTS_DIR} and try again."
-    exit 1
-fi
-
-# Cleanup
-rm -rf src/contracts
-mkdir src/contracts
+# Init submodule
+git submodule init ${CONTRACTS_DIR}
 
 echo "Compile contracts and generate types"
-
 DAPP_DIR=$(pwd)
 cd ${CONTRACTS_DIR}
 
-npx hardhat compile
+pnpm i
+pnpx hardhat compile
 
 if [ $? -ne 0 ]; then
-    echo "Something went wrong executing the 'npx hardhat' command, did you run 'npm i' in ${CONTRACTS_DIR}?"
+    echo "Something went wrong executing the 'hardhat' command, did you run 'pnpm i' in ${CONTRACTS_DIR}?"
+    exit 1
 fi
 
 cp -r typechain/* ${DAPP_DIR}/src/contracts
