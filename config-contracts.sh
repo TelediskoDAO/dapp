6b4d78f8bbecbbd5d294bd263c6c1917598a23f3
vrde <agranzot@mailbox.org>
2022-02-15 15:08:05 +0100

    Pin submodule commit, deploy to another repo

--- config-contracts.sh
+++ config-contracts.sh
@@ -3,21 +3,15 @@
 CONTRACTS_DIR="contracts";
 
 # Init submodule
-git submodule init ${CONTRACTS_DIR}
+git submodule update --init ${CONTRACTS_DIR}
 
-echo "Compile contracts and generate types"
 DAPP_DIR=$(pwd)
 cd ${CONTRACTS_DIR}
-
 pnpm i
 pnpx hardhat compile
 
-if [ $? -ne 0 ]; then
-    echo "Something went wrong executing the 'hardhat' command, did you run 'pnpm i' in ${CONTRACTS_DIR}?"
-    exit 1
-fi
-
-cp -r typechain/* ${DAPP_DIR}/src/contracts
+echo "Copy types"
+cp -r typechain/* ../src/contracts
 
 echo "Copy network configuration"
-cp deployments/* ${DAPP_DIR}/src/contracts
+cp deployments/* ../src/contracts
