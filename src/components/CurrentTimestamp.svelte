926c0347734b9ee039e1f6204f4d7f5f85ad3ffe
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2022-02-27 00:21:00 +0100

    fix: removed async

--- src/components/CurrentTimestamp.svelte
+++ src/components/CurrentTimestamp.svelte
@@ -6,7 +6,7 @@
   let interval = null;
   export let intervalMs = 5000;
 
-  onMount(async () => {
+  onMount(() => {
     interval = setInterval(() => {
       $currentTimestamp = +new Date();
     }, intervalMs);
