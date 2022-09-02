ab2450a655ea0a6780c7832c8782ad761661dfb9
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2022-09-02 18:08:26 +0200

    feat: new widget for resolution type

---
+++ src/i18n/resolution.ts
@@ -0,0 +1,42 @@
+export const RESOLUTION_TYPES_TEXTS = {
+  amendment: {
+    title: "Amendment",
+    description:
+      "Bruce Wayne, eccentric billionaire. My anger outweights my guilt. Bats frighten me. It's time my enemies shared my dread. Does it come in black? It's not who I am underneath but what I do that defines me.",
+  },
+  capitalChange: {
+    title: "Capital Change",
+    description:
+      "Bruce Wayne, eccentric billionaire. My anger outweights my guilt. Bats frighten me. It's time my enemies shared my dread. Does it come in black? It's not who I am underneath but what I do that defines me.",
+  },
+  preclusion: {
+    title: "Preclusion",
+    description:
+      "Bruce Wayne, eccentric billionaire. My anger outweights my guilt. Bats frighten me. It's time my enemies shared my dread. Does it come in black? It's not who I am underneath but what I do that defines me.",
+  },
+  fundamentalOther: {
+    title: "Fundamental / Other",
+    description:
+      "Bruce Wayne, eccentric billionaire. My anger outweights my guilt. Bats frighten me. It's time my enemies shared my dread. Does it come in black? It's not who I am underneath but what I do that defines me.",
+  },
+  significant: {
+    title: "Significant",
+    description:
+      "Bruce Wayne, eccentric billionaire. My anger outweights my guilt. Bats frighten me. It's time my enemies shared my dread. Does it come in black? It's not who I am underneath but what I do that defines me.",
+  },
+  dissolution: {
+    title: "Dissolution",
+    description:
+      "Bruce Wayne, eccentric billionaire. My anger outweights my guilt. Bats frighten me. It's time my enemies shared my dread. Does it come in black? It's not who I am underneath but what I do that defines me.",
+  },
+  routine: {
+    title: "Routine",
+    description:
+      "Bruce Wayne, eccentric billionaire. My anger outweights my guilt. Bats frighten me. It's time my enemies shared my dread. Does it come in black? It's not who I am underneath but what I do that defines me.",
+  },
+  genesis: {
+    title: "Genesis",
+    description:
+      "Bruce Wayne, eccentric billionaire. My anger outweights my guilt. Bats frighten me. It's time my enemies shared my dread. Does it come in black? It's not who I am underneath but what I do that defines me.",
+  },
+};
