b02982c06677cfdac640b1d4e5794bdfc4c975c1
vrde <agranzot@mailbox.org>
2022-12-24 13:54:03 +0100

    fix: show all tasks

--- src/pages/neokingdom/task/Tasks.svelte
+++ src/pages/neokingdom/task/Tasks.svelte
@@ -1,12 +1,10 @@
 <script>
   import { parse } from "qs";
   import { afterUpdate } from "svelte";
+  import Paper, { Title, Content } from "@smui/paper";
   import { replace, querystring } from "svelte-spa-router";
   import { tasks, currentTask } from "../../../state/neokingdom/odoo";
   import Task from "./Task.svelte";
-  import Foldable from "../../../components/Foldable.svelte";
-  import Accordion, { Content, Header, Panel } from "@smui-extra/accordion";
-  import IconButton, { Icon } from "@smui/icon-button";
 
   export let list;
   export let openDetails = false;
@@ -46,48 +44,42 @@
 </script>
 
 {#each sortedList as project}
-  <Accordion>
-    <Panel>
-      <Header>
-        {project.name}
-        <IconButton slot="icon" toggle>
-          <Icon class="material-icons" on>expand_less</Icon>
-          <Icon class="material-icons">expand_more</Icon>
-        </IconButton>
-      </Header>
-      <Content>
-        <div class="metadata">
-          {#if project.stagesCount.done > 0}
-            <p>
-              <span
-                class="toggle"
-                on:click|stopPropagation={toggleDone.bind(null, project.id)}
-                >{stages[project.id].includes("done") ? "Hide" : "Show"}
-                completed tasks</span
-              >
-            </p>
-          {/if}
-        </div>
-        {#if project.taskIds.length}
-          <ul>
-            {#each project.taskIds as taskId}
-              {#if $tasks[taskId] && $tasks[taskId].isParentTask}
-                <li>
-                  <Task
-                    stages={stages[project.id]}
-                    task={$tasks[taskId]}
-                    {openDetails}
-                  />
-                </li>
-              {/if}
-            {/each}
-          </ul>
-        {:else}
-          <p>No tasks.</p>
+  <Paper>
+    <Title>
+      {project.name}
+    </Title>
+    <Content>
+      <div class="metadata">
+        {#if project.stagesCount.done > 0}
+          <p>
+            <span
+              class="toggle"
+              on:click|stopPropagation={toggleDone.bind(null, project.id)}
+              >{stages[project.id].includes("done") ? "Hide" : "Show"}
+              completed tasks</span
+            >
+          </p>
         {/if}
-      </Content>
-    </Panel>
-  </Accordion>
+      </div>
+      {#if project.taskIds.length}
+        <ul>
+          {#each project.taskIds as taskId}
+            {#if $tasks[taskId] && $tasks[taskId].isParentTask}
+              <li>
+                <Task
+                  stages={stages[project.id]}
+                  task={$tasks[taskId]}
+                  {openDetails}
+                />
+              </li>
+            {/if}
+          {/each}
+        </ul>
+      {:else}
+        <p>No tasks.</p>
+      {/if}
+    </Content>
+  </Paper>
 {/each}
 
 <style>
